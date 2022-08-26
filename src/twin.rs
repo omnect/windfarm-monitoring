684f9aef6392559a26926ffa85ed3dd99e93a029
JanZachmann <jan.zachmann@conplement.de>
2022-08-26 09:09:37 +0200

    - report azure-sdk-version in twin  - log info message for azure-sdk-version  - bump to azure-iot-sdk 0.8.4

--- src/twin.rs
+++ src/twin.rs
@@ -12,12 +12,13 @@ pub fn update(
     Ok(())
 }
 
-pub fn report_version(tx_app2client: Arc<Mutex<Sender<Message>>>) -> Result<(), IotError> {
+pub fn report_versions(tx_app2client: Arc<Mutex<Sender<Message>>>) -> Result<(), IotError> {
     tx_app2client
         .lock()
         .unwrap()
         .send(Message::Reported(json!({
-            "module-version": env!("CARGO_PKG_VERSION")
+            "module-version": env!("CARGO_PKG_VERSION"),
+            "azure sdk version": IotHubClient::get_sdk_version_string()
         })))?;
 
     Ok(())
