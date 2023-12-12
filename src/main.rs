baffcbebdd4a1ded687cd54893b6976fe8ad4aac
JoergZeidler <62105035+JoergZeidler@users.noreply.github.com>
2023-12-12 08:28:44 +0100

    error log messages are forced to be sent to STDERR (#30)
    - all other log messages are sent to STDOUT
    - bumbed env_logger to 0.10

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-pub mod twin;
 mod metrics_provider;
+pub mod twin;
 
 use azure_iot_sdk::client::*;
-use env_logger::{Builder, Env};
+use env_logger::{Builder, Env, Target};
 use log::{error, info};
+use std::io::Write;
 use std::process;
 use twin::Twin;
 
 #[tokio::main]
 async fn main() {
+    let mut builder;
     log_panics::init();
 
     if cfg!(debug_assertions) {
-        Builder::from_env(Env::default().default_filter_or("debug")).init();
+        builder = Builder::from_env(Env::default().default_filter_or("debug"));
     } else {
-        Builder::from_env(Env::default().default_filter_or("info")).init();
+        builder = Builder::from_env(Env::default().default_filter_or("info"));
     }
+
+    builder.format(|f, record| match record.level() {
+        log::Level::Error => {
+            eprintln!("{}", record.args());
+            Ok(())
+        }
+        _ => {
+            writeln!(f, "{}", record.args())
+        }
+    });
+
+    builder.target(Target::Stdout).init();
 
     info!("module version: {}", env!("CARGO_PKG_VERSION"),);
     info!("azure sdk version: {}", IotHubClient::sdk_version_string());
